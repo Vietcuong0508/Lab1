8f923ca82ca24ed510a8f8fd7601a36dc9d7f079
Vietcuong0508 <73566409+Vietcuong0508@users.noreply.github.com>
2021-06-02 13:54:37 +0700

    update lap1

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Ex4
+﻿using System;
+
+namespace Ex4
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("\nEnter n: ");  
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(x + " " + "x" + " " + i + " " + "=" + " " + (x*i));
+            }
         }
     }
 }
---
+++ Ex5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                for (int num = 1; num < 21; num++)
+                {
+                    Console.WriteLine("Factorial of "
+                                      + num + " is " + factorial(num));
+                }
+            }
+        }
+
+        static long factorial(long n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * factorial(n - 1);
+        }
+    }
+}
